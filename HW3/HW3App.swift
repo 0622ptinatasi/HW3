a33bedb860f751be7afc1d192540a4656808d84e
hw2 <tinatina62027@gmail.com>
2024-11-13 20:11:45 +0800

    Initial Commit

---
+++ HW3/HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3App.swift
+//  HW3
+//
+//  Created by user12 on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
