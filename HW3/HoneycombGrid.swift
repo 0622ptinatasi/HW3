dab7856bbddd6e1eebd992d985a6e5144fdaf234
hw2 <tinatina62027@gmail.com>
2024-11-26 16:37:32 +0800

    1

---
+++ HW3/HoneycombGrid.swift
@@ -0,0 +1,18 @@
+//
+//  HoneycombGrid.swift
+//  HW3
+//
+//  Created by user12 on 2024/11/16.
+//
+
+import SwiftUI
+
+struct HoneycombGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HoneycombGrid()
+}
---
+++ HW3/SetView.swift
@@ -0,0 +1,18 @@
+//
+//  SetView.swift
+//  HW3
+//
+//  Created by user12 on 2024/11/21.
+//
+
+import SwiftUI
+
+struct SetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetView()
+}
